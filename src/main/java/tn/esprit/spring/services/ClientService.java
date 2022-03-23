0c68d5f37be944562136491c27a11b231b237a00
Wissem LEMSI <WLE@iptech.corp>
2022-03-23 14:33:49 +0100

    added an important line of code

--- src/main/java/tn/esprit/spring/services/ClientService.java
+++ src/main/java/tn/esprit/spring/services/ClientService.java
@@ -5,5 +5,6 @@ import tn.esprit.spring.entities.Client;
 public interface ClientService {
 	
 	void ajouterEtAffecterClientBoutiques(Client client, List<Long> idBoutiques) ;
+	/*hehehehehehehehehe xD */
 
 }
