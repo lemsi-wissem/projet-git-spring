318a40f28d7f059f8c57646a44fd0621ecc9d150
oussema.trojet1 <oussema.trojet1@gmail.com>
2022-03-23 14:21:13 +0100

    ajout commentaire in boutiqueServiceImpl

--- src/main/java/tn/esprit/spring/services/BoutiqueServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/BoutiqueServiceImpl.java
@@ -26,6 +26,7 @@ public class BoutiqueServiceImpl implements BoutiqueService {
 		for(Boutique boutique:lb) {
 			boutique.setCentreCommercial(c);
 			boutiqueRepo.save(boutique);
+			// commentaire oussema 1
 		}
 	}
 
